17aa846d3bc9c9b20e60166970a253c9b5dff478
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-01-29 19:23:13 -0600

    fixed api route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,41 +1,7 @@
 var db = require("../models");
 
 module.exports = function(app) {
-    // // GET ROUTES
-    // app.get("/api/workouts", function(req, res) {
-    //     db.Workout.find({}).pretty().then(function(dbworkout) {
-    //             res.json(dbworkout);
-    //         })
-    //         .catch(err => {
-    //             res.status(400).json(err);
-    //         });
-    // });
-    // app.get("/api/workouts/range", function(req, res) {
-    //     db.Workout.find({}).pretty().limit(7).then(function(dbworkout) {
-    //             res.json(dbworkout);
-    //         })
-    //         .catch(err => {
-    //             res.status(400).json(err);
-    //         });
-    // });
-    // // UPDATE ROUTE
-    // app.put("/api/workouts", function(req, res) {
-    //     db.Workout.insert({}).then(result => {
-    //             res.json(result);
-    //         })
-    //         .catch(err => {
-    //             res.status(400).json(err);
-    //         });
-    // });
-    // //POST ROUTE
-    // app.post("/api/workouts", function(req, res) {
-    //     db.Workout.create(req.body).then(function(dbworkout) {
-    //             res.json(dbworkout);
-    //         })
-    //         .catch(err => {
-    //             res.status(400).json(err);
-    //         });
-    // });
+
     // GET ROUTES
     app.get("/api/workouts", function(req, res) {
         db.Workout.find({}).then(function(dbWorkouts) {
