29853d8d11ca9b58002878693d78b6c317486557
Jacob Rosenbaum <jacobrosenbaum95@gmail.com>
2020-02-06 16:21:00 -0600

    PWA working offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,8 @@
-const FILES_TO_CACHE = ["/", "/index.html", "stats.js", "exercise.html", "exercise.js", "stats.html", "index.js", "api.js"]
+const FILES_TO_CACHE = ["/", "/index.html", "/assets/js/stats.js", "/exercise.html", "/assets/js/exercise.js", "/stats.html", "/assets/js/api.js", "/assets/css/style.css", "assets/css/workout-style.css", "/icon-512x512.png"]
 
-const CACHE_NAME = "static-cache-v2";
-const DATA_CACHE_NAME = "data-cache-v1";
+
+const CACHE_NAME = "static-cache-v4";
+const DATA_CACHE_NAME = "data-cache-v4";
 
 // install
 self.addEventListener("install", function(evt) {
@@ -35,6 +36,27 @@ self.addEventListener("activate", function(evt) {
 
 // fetch
 self.addEventListener("fetch", function(evt) {
+    if (evt.request.url.includes("/api/")) {
+        evt.respondWith(
+            caches.open(DATA_CACHE_NAME).then(cache => {
+                return fetch(evt.request)
+                    .then(response => {
+                        // If the response was good, clone it and store it in the cache.
+                        if (response.status === 200) {
+                            cache.put(evt.request.url, response.clone());
+                        }
+                        return response;
+                    })
+                    .catch(err => {
+                        // Network request failed, try to get it from the cache.
+                        return cache.match(evt.request);
+                    });
+            }).catch(err => {
+                console.log(err)
+            })
+        );
+        return;
+    }
     evt.respondWith(
         caches.open(CACHE_NAME).then(cache => {
             return cache.match(evt.request).then(response => {
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,5 @@
 var path = require("path");
-var public = require("../public");
+// var public = require("../public");
 
 module.exports = function(app) {
     // Load index page
